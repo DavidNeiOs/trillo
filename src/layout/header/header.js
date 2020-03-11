8259c0222b7dff39c1e46f019d0a2d0f2895c5ac
DavidNeiOs <davidneios25@gmail.com>
2020-03-11 01:36:49 -0400

    search bar and logo

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,5 +1,42 @@
 import React from "react";
+import Logo from "../../img/logo.png";
+import sprite from "../../img/sprite.svg";
+import userPhoto from "../../img/user.jpg";
 
 export const Header = props => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <img src={Logo} alt="Trillo logo" className="logo" />
+
+      <form onSubmit={e => e.preventDefault()} className="search">
+        <input
+          type="text"
+          className="search__input"
+          placeholder="Search Hotels"
+        />
+        <button className="search__button">
+          <svg className="search__icon">
+            <use xlinkHref={`${sprite}#icon-magnifying-glass`}></use>
+          </svg>
+        </button>
+      </form>
+
+      <nav className="user-nav">
+        <div className="user-nav__icon-box">
+          <svg className="user-nav__icon">
+            <use xlinkHref={`${sprite}#icon-bookmark`}></use>
+          </svg>
+          <span className="user-nav__notification">7</span>
+          <svg className="user-nav__icon">
+            <use xlinkHref={`${sprite}#icon-chat`}></use>
+          </svg>
+          <span className="user-nav__notification">13</span>
+        </div>
+        <div className="user-nav__user">
+          <img src={userPhoto} alt="User" className="user-nav__user-photo" />
+          <span className="user-nav__user-name">Jonas</span>
+        </div>
+      </nav>
+    </header>
+  );
 };
