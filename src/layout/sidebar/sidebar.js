b5f8a628e7e9f718dd4164e95cca81089c229377
DavidNeiOs <davidneios25@gmail.com>
2020-03-11 11:29:25 -0400

    sidebar done

--- src/layout/sidebar/sidebar.js
+++ src/layout/sidebar/sidebar.js
@@ -1,5 +1,45 @@
 import React from "react";
+import sprite from "../../img/sprite.svg";
 
 export const Sidebar = props => {
-  return <nav className="sidebar">Sidebar</nav>;
+  return (
+    <nav className="sidebar">
+      <ul className="side-nav">
+        <li className="side-nav__item side-nav__item--active">
+          <a href="#" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-home`}></use>
+            </svg>
+            <span>Hotel</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="#" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-aircraft-take-off`}></use>
+            </svg>
+            <span>Flight</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="#" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-key`}></use>
+            </svg>
+            <span>Car rental</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="#" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-map`}></use>
+            </svg>
+            <span>tours</span>
+          </a>
+        </li>
+      </ul>
+
+      <div className="legal">&copy; 2020 by Trillo. All rights reserved.</div>
+    </nav>
+  );
 };
