1bdb349d8f125e986605d795757f708af28cf6b2
DavidNeiOs <davidneios25@gmail.com>
2020-03-11 18:45:37 -0400

    cta section added

--- src/pages/hotel/hotel.js
+++ src/pages/hotel/hotel.js
@@ -125,6 +125,16 @@ export const HotelPage = props => {
           </button>
         </div>
       </div>
+      <div className="cta">
+        <h2 className="cta__book-now">
+          Good news ! We have 4 free rooms for your selected dates!
+        </h2>
+
+        <button className="btn">
+          <span className="btn__visible">Book now</span>
+          <span className="btn__invisible">Only 4 rooms left</span>
+        </button>
+      </div>
     </main>
   );
 };
